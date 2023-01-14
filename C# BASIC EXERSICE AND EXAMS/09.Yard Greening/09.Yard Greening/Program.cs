c25ff58c769ab8af35b0a0f6e58c4bfa420d4350
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-14 21:01:11 +0200

    fix

--- C# BASIC EXERSICE AND EXAMS/09.Yard Greening/09.Yard Greening/Program.cs
+++ C# BASIC EXERSICE AND EXAMS/09.Yard Greening/09.Yard Greening/Program.cs
@@ -13,7 +13,7 @@ namespace CatalogVehicle
             double meetersGrass = double.Parse(Console.ReadLine());
 
             double priceMeeterGrass = (7.61 * meetersGrass) * 0.82;
-            double discount = meetersGrass * 7.61 * 0.18;
+            double discount = meetersGrass * 7.61 * 0.68;
             Console.WriteLine($"The final price is :{priceMeeterGrass:F2} lv.");
             Console.WriteLine($"The discount is :{discount:F2} lv.");
 
