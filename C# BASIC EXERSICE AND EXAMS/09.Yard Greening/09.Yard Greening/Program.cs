ccf02b4426811c09a3beda260748e837043050a0
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-14 20:43:40 +0200

    exercise

---
+++ C# BASIC EXERSICE AND EXAMS/09.Yard Greening/09.Yard Greening/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace CatalogVehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double meetersGrass = double.Parse(Console.ReadLine());
+
+            double priceMeeterGrass = (7.61 * meetersGrass) * 0.82;
+            double discount = meetersGrass * 7.61 * 0.18;
+            Console.WriteLine($"The final price is :{priceMeeterGrass:F2} lv.");
+            Console.WriteLine($"The discount is :{discount:F2} lv.");
+
+        }
+    }
+}
