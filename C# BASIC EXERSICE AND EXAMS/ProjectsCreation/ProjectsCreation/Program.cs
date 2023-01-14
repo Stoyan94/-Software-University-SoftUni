e74ecdf1ef9e4a96e0dea2f4c2f19f45a6335fba
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-14 20:09:16 +0200

    Project

---
+++ C# BASIC EXERSICE AND EXAMS/ProjectsCreation/ProjectsCreation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace CatalogVehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameArhi = Console.ReadLine();
+            int numProjects = int.Parse(Console.ReadLine());
+
+            int hoursForProject = numProjects * 3;
+
+            Console.WriteLine($"The architect {nameArhi} will need {hoursForProject} " +
+                $"hours to complete {numProjects} project/s.");
+
+        }
+    }
+}
