ca051a6f575966e8bf58c9e4c12c360fe5fa1b6c
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-15 20:50:28 +0200

    Exercise

---
+++ C# BASIC EXERSICE AND EXAMS/ConditionalStatements-EX.01/ConditionalStatements-EX.01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace demo_15_min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statis = int.Parse(Console.ReadLine());
+            double oneStatPrice = double.Parse(Console.ReadLine());
+            double dekor = (budget * 0.1);
+            double budgetNeedet = budget * 0.1;
+
+            if (statis > 150)
+            {
+                budgetNeedet += statis * oneStatPrice * 0.9;
+            }
+            else
+            {
+                budgetNeedet += statis * oneStatPrice;
+            }
+            if (budgetNeedet > budget)
+            {
+                double result = budgetNeedet - budget;
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {result:F2} leva more.");
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                double result = budget - budgetNeedet;
+                Console.WriteLine($"Wingard starts filming with {result:F2} leva left.");
+            }
+        }
+    }
+}
