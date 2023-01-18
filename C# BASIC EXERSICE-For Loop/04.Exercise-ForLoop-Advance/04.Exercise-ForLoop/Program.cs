0ac33ab021af201d4417365648f7aedd2a26a1bb
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-18 19:48:16 +0200

    Exercise

---
+++ C# BASIC EXERSICE-For Loop/04.Exercise-ForLoop-Advance/04.Exercise-ForLoop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.Exercise_ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumNums = 0;
+            int max = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sumNums += num;
+
+                if (num > max)
+                {
+                    max = num;
+                }
+
+            }
+            int allNums = sumNums - max;
+            if (max == allNums)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {max}");
+            }
+            else
+            {
+                int diff = Math.Abs(max - allNums);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+
+        }
+    }
+}
