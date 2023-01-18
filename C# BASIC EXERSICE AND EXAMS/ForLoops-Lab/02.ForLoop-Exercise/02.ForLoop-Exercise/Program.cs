14c47820e135e8ef4e5103bf64932f917ea9dc97
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-18 18:56:22 +0200

    Exercise

---
+++ C# BASIC EXERSICE AND EXAMS/ForLoops-Lab/02.ForLoop-Exercise/02.ForLoop-Exercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rigthSum = 0;
+
+            for (int i = 0; i < 2 * n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (i < n)
+                {
+                    leftSum += num;
+                }
+
+                else
+                {
+                    rigthSum += num;
+                }
+            }
+            if (leftSum == rigthSum)
+            {
+
+                Console.WriteLine($" Yes, sum = {leftSum}");
+            }
+            else
+            {
+                Console.WriteLine($" No, diff = {Math.Abs(leftSum - rigthSum)}");
+            }
+
+
+        }
+    }
+}
