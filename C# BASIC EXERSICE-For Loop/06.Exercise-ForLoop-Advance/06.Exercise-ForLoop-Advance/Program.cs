8f5db0e66a2290b9f2e1e8cf8a3f3dfd2eccd166
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-19 23:32:09 +0200

    Exercise

---
+++ C# BASIC EXERSICE-For Loop/06.Exercise-ForLoop-Advance/06.Exercise-ForLoop-Advance/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _06.Exercise_ForLoop_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearLili = int.Parse(Console.ReadLine());
+            double priceWasher = double.Parse(Console.ReadLine());
+            int toysCount = int.Parse(Console.ReadLine());
+
+            double evenBird = 0;
+            double toysPrice = 0;
+
+            double totalEven = 0;
+
+            for (int i = 1; i <= yearLili; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    evenBird += 10;
+                    totalEven = (totalEven + evenBird) - 1;
+                }
+                else
+                {
+                    toysPrice += toysCount;
+                }
+
+            }
+
+            double totalPrice = totalEven + toysPrice;
+
+            if (totalPrice >= priceWasher)
+            {
+                Console.WriteLine($"Yes! {totalPrice - priceWasher:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {priceWasher - totalPrice:F2}");
+            }
+        }
+    }
+}
