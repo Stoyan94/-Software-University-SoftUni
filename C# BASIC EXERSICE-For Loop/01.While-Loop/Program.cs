72b9b8687ecdfc61508bd57cd1d972b771f4e499
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-22 16:45:20 +0200

    Exercise

---
+++ C# BASIC EXERSICE-For Loop/01.While-Loop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01.While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double totalMoney = 0;
+            while (input != "NoMoreMoney")
+            {
+                double transaction = double.Parse(input);
+                if (transaction <= 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {transaction:F2}");
+                totalMoney += transaction;
+
+
+                input = Console.ReadLine();
+
+            }
+            Console.WriteLine($"Total: {totalMoney:F2}");
+        }
+    }
+}
