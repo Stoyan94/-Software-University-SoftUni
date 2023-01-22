fbbdcc1038e95d9229d74b99d603a9232826e731
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-22 16:39:19 +0200

    EX

---
+++ C# BASIC EXERSICE-For Loop/08.Exercise-ForLoop-Advance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _08.Exercise_ForLoop_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tourCount = int.Parse(Console.ReadLine());
+            int startPoints = int.Parse(Console.ReadLine());
+            int pointsWon = 0;
+            int torWon = 0;
+
+            for (int i = 0; i < tourCount; i++)
+            {
+                string typeTor = Console.ReadLine();
+
+                if (typeTor == "W")
+                {
+                    pointsWon += 2000;
+                    torWon++;
+                }
+                else if (typeTor == "F")
+                {
+                    pointsWon += 1200;
+                }
+                else if (typeTor == "SF")
+                {
+                    pointsWon += 720;
+                }
+            }
+            Console.WriteLine($"Final points: {startPoints + pointsWon}");
+            int averagePoits = pointsWon / tourCount;
+            Console.WriteLine($"Average points: {averagePoits}");
+            double preTour = (double)torWon / tourCount * 100;
+            Console.WriteLine($"{preTour:F2}%");
+        }
+    }
+}
