6d6abcbb8d8a191a8365aa415e2db76f3f389dff
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-22 17:02:58 +0200

    Exercise

---
+++ While Loop-EXERCISES/03.While-Loop-Advance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _03.While_Loop_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bookSearch = Console.ReadLine();
+            int countBook = 0;
+            bool isFound = false;
+            string booksCeck = Console.ReadLine();
+
+            while (booksCeck != "No More Books")
+            {
+                if (booksCeck == bookSearch)
+                {
+                    isFound = true;
+                    break;
+                }
+                else
+                {
+                    countBook++;
+                }
+                booksCeck = Console.ReadLine();
+            }
+
+
+            if (isFound)
+            {
+                Console.WriteLine($"You checked {countBook} books and found it.");
+
+            }
+            else
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {countBook} books.");
+            }
+        }
+    }
+}
