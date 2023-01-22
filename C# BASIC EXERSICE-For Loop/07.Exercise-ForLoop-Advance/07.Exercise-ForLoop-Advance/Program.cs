a04cfdc2d81ddd8c81fd6f2746e314ab079d3401
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-22 16:36:14 +0200

    Exercise

---
+++ C# BASIC EXERSICE-For Loop/07.Exercise-ForLoop-Advance/07.Exercise-ForLoop-Advance/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _07.Exercise_ForLoop_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                string TabOpen = Console.ReadLine();
+
+                if (TabOpen == "Facebook")
+                {
+                    salary -= 150;
+                }
+                else if (TabOpen == "Instagram")
+                {
+                    salary -= 100;
+                }
+                else if (TabOpen == "Reddit")
+                {
+                    salary -= 50;
+                }
+                else if (salary == 0)
+                {
+                    break;
+                }
+            }
+            if (salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
