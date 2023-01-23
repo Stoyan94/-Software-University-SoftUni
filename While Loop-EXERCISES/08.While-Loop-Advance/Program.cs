35963b31d8f33324d712d421622195fa814b4406
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-23 22:36:57 +0200

    ex

---
+++ While Loop-EXERCISES/08.While-Loop-Advance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _08.While_Loop_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+
+            int cakceSize = width * lenght;
+
+            string pieces = Console.ReadLine();
+
+            while (pieces != "STOP")
+            {
+                int takePieces = int.Parse(pieces);
+
+
+                cakceSize -= takePieces;
+
+                if (cakceSize <= 0)
+                {
+                    break;
+                }
+
+                pieces = Console.ReadLine();
+            }
+
+            if (cakceSize <= 0)
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(cakceSize)} pieces more.");
+            }
+            else
+            {
+                Console.WriteLine($"{cakceSize} pieces are left.");
+            };
+        }
+    }
+}
