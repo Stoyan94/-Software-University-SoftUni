dac6d815bf3d6220c8efbd1d2428dc5b63799b14
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-24 20:21:00 +0200

    Ex

---
+++ Nested Loops/03.Nested-Loops/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03.Nested_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while (destination != "End")
+            {
+                double expences = double.Parse(Console.ReadLine());
+
+                double saving = 0;
+
+                while (expences > saving)
+                {
+                    saving += double.Parse(Console.ReadLine());
+                }
+                Console.WriteLine($"Going to {destination}!");
+
+                destination = Console.ReadLine();
+            }
+        }
+    }
+}
