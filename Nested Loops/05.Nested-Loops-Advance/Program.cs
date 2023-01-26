a89bde0323f9f5fa8b0d4e5422c6b73c020b6ccd
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-26 19:12:46 +0200

    ex

---
+++ Nested Loops/05.Nested-Loops-Advance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _05.Nested_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int current = 1;
+
+            bool isBigger = false;
+
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int nums = 1; nums <= rows; nums++)
+                {
+                    if (current > n)
+                    {
+                        isBigger = true;
+
+                        break;
+                    }
+
+                    Console.Write(current + " ");
+
+                    current++;
+                }
+
+                if (isBigger)
+                {
+                    break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
