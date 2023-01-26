9c2c58b85599f0313dab9c073e69f2ddc6607109
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-26 19:15:23 +0200

    ex

---
+++ Nested Loops/06.Nested-Loops-Advance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06.Nested_Loops_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                int currentNum = i;
+
+                int evenSum = 0;
+                int oddSum = 0;
+
+                for (int j = 0; j < 6; j++)
+                {
+                    int digit = currentNum % 10;
+
+                    if (j % 2 == 0)
+                    {
+                        evenSum += digit;
+                    }
+                    else
+                    {
+                        oddSum += digit;
+                    }
+                    currentNum /= 10;
+                }
+
+                if (evenSum == oddSum)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
