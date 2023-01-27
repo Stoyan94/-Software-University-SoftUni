597b1b28cc3ed2bb45a9463d13616ad0fd73e28c
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-27 22:56:13 +0200

    ex

---
+++ Nested Loops/08.Nested-Loops-Advance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _08.Nested_Loops_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int juri = int.Parse(Console.ReadLine());  
+            string present = Console.ReadLine();
+
+            double studentDegree = 0;
+
+            double presentSum = 0;
+            double evaluationSum = 0;
+
+            while (present != "Finish")
+            {
+
+                for (double i = 0; i < juri; i++)
+                {
+                    double evaluation = double.Parse(Console.ReadLine());
+
+                    studentDegree += evaluation;
+                    presentSum += evaluation;
+
+                    evaluationSum++;
+                }
+
+                double precentageDegree = (double)studentDegree / juri;
+
+                Console.WriteLine($"{present} - {precentageDegree:F2}.");
+
+                studentDegree = 0;
+                present = Console.ReadLine();
+            }
+
+            double averageSum = presentSum / evaluationSum;
+            Console.WriteLine($"Student's final assessment is {averageSum:F2}.");
+        }
+    }
+}
