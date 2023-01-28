23b89a2d2128f85c017d58a7811728562aab4209
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-28 10:34:21 +0200

    ex

---
+++ Nested Loops/09.Nested-Loops-Advance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _09.Nested_Loops_Advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1111; i < 9999; i++)
+            {
+                int currentNum = i;
+                bool isSpecial = true;
+
+                for (int j = 0; j < 4; j++)
+                {
+                    double digit = currentNum % 10;
+
+                    if (num % digit == 0)
+                    {
+
+                    }
+                    else if (num % digit != 0)
+                    {
+                        isSpecial = false;
+                        break;
+                    }
+                    currentNum /= 10;
+                }
+
+                if (isSpecial)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
