d3528ff0e81cfadf0f69e6de6d6bef0cf8e18c6b
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-30 12:02:38 +0200

    ex

---
+++ Exam Preparation/05.Best-Player/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _05.Best_Player
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerName = Console.ReadLine();
+
+            double goalsScore = 0;
+            string bestPlayr = "";
+
+            while (playerName != "END")
+            {
+                int goals = int.Parse(Console.ReadLine());
+
+                if (goals == goalsScore)
+                {
+                    break;
+                }
+                else if (goals >= 10)
+                {
+                    bestPlayr = playerName;
+                    goalsScore = goals;
+                    break;
+                }
+                else if (goals >= 3 && goals < 10)
+                {
+                    bestPlayr = playerName;
+                    goalsScore = goals;
+                }
+                else if (goals < 3)
+                {
+                    bestPlayr = playerName;
+                    goalsScore = goals;
+                }
+                playerName = Console.ReadLine();
+
+            }
+            Console.WriteLine($"{bestPlayr} is the best player!");
+
+            if (goalsScore < 3)
+            {
+                Console.WriteLine($"He has scored {goalsScore} goals.");
+            }
+            else if (goalsScore >= 3 && goalsScore < 10)
+            {
+                Console.WriteLine($"He has scored {goalsScore} goals and made a hat-trick !!!");
+            }
+            else if (goalsScore >= 10)
+            {
+                Console.WriteLine($"He has scored {goalsScore} goals and made a hat-trick !!!");
+            }
+        }
+    }
+}
