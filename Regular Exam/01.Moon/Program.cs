c5ceedf8c611a5c34de8dad853cd61e7d0cfb024
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-01-31 12:25:55 +0200

    ex

---
+++ Regular Exam/01.Moon/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01.Moon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double speedKm = double.Parse(Console.ReadLine());
+            double fuel = double.Parse(Console.ReadLine());
+            double distance = 384400;
+
+            double fullTime = distance * 2;
+            double timeBack = Math.Ceiling(fullTime / speedKm);
+            double sumTime = timeBack + 3;
+            double sumFuel = (fuel * fullTime) / 100;
+
+            Console.WriteLine($"{Math.Ceiling(sumTime)}");
+            Console.WriteLine(sumFuel);
+
+        }
+    }
+}
