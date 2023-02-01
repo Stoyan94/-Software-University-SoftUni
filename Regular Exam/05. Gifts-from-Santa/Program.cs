d3eb396fcd731bd88946163790d7c7954c299dde
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-01 22:40:29 +0200

    ex

---
+++ Regular Exam/05. Gifts-from-Santa/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05._Gifts_from_Santa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+
+            for (int i = m; i >= n; i--)
+            {
+
+                if (i % 3 == 0 && i % 2 == 0 && i == s)
+                {
+                    break;
+                }
+                if (i % 3 == 0 && i % 2 == 0)
+                {
+
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
